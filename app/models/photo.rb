ee718a64f329d44afaad33caf8880ad7fa407a7c
Christopher Hopkins <chris@chocol.it>
2013-08-29 00:03:24 -0400

    add photo resource

---
+++ app/models/photo.rb
@@ -0,0 +1,3 @@
+class Photo < ActiveRecord::Base
+  attr_accessible :source_url
+end
---
+++ spec/factories/photos.rb
@@ -0,0 +1,7 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :photo do
+    source_url "MyString"
+  end
+end
