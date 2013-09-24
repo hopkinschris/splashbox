ce80832f636829fcf40cde1c222425f49f60e11b
Christopher Hopkins <chris@chocol.it>
2013-09-23 20:38:59 -0400

    add public waitlist index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  def index
+  end
+
   def show
     @user = User.find_by_id(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Splashbox::Application.routes.draw do
   root "home#show"
 
+  get '/waitlist',  to: 'home#index', as: :waitlist
+
   match 'auth',     via: [:post], to: 'dropbox#authorize', as: :auth
   match 'callback', via: [:get],  to: 'dropbox#authorized_callback', as: :callback
 end
